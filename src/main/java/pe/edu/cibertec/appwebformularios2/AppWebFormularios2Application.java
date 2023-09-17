d7bc307cb1da324f894270adf5b23048ec9d28d8
lsalvatierra89 <l.salvat@gmail.com>
2023-09-17 14:17:22 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/appwebformularios2/AppWebFormularios2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformularios2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebFormularios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebFormularios2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebformularios2/AppWebFormularios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebformularios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFormularios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
